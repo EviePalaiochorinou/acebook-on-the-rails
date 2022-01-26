b2d069ac91875c02d975b5e115a8d31a64a2ea32
Sarah <dousse.sarah@gmail.com>
2022-01-26 23:24:34 +0000

    listing friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,17 @@
 class FriendsController < ApplicationController
+  before_action :friend_setup, only: :destroy
+
   def index
+    @friends = current_user.friends
   end
 
   def destroy
+
+  end
+
+  private
+
+  def friend_setup
+    @friend = current_user.friends.find(params[:id])
   end
 end
